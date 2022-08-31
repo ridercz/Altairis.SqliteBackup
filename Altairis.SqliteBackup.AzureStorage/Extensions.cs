230c98a65723258e4bb70edaf9a449c1356f31b3
Michal Altair Valasek <michal.valasek@altairis.cz>
2022-09-01 01:36:14 +0200

    Create and use generic WithProcessor<TProcessor>() method,

--- Altairis.SqliteBackup.AzureStorage/Extensions.cs
+++ Altairis.SqliteBackup.AzureStorage/Extensions.cs
@@ -7,7 +7,6 @@ public static class Extensions {
     public static BackupServiceBuilder WithAzureStorageUpload(this BackupServiceBuilder builder, string connectionString, Action<AzureStorageUploadProcessorOptions>? configureOptions = null) {
         var options = new AzureStorageUploadProcessorOptions(connectionString);
         configureOptions?.Invoke(options);
-        builder.Services.AddSingleton<IBackupProcessor>(sp => new AzureStorageUploadProcessor(options, sp.GetRequiredService<ILogger<AzureStorageUploadProcessor>>()) { Priority = builder.NextProcessorPriority });
-        return builder.Next();
+        return builder.WithProcessor<AzureStorageUploadProcessor>(sp => new AzureStorageUploadProcessor(options, sp.GetRequiredService<ILogger<AzureStorageUploadProcessor>>()));
     }
 }
